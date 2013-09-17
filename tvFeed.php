f1c21919a6af05471289b1c2a98527a70058ab61
Justin Scott <jjustin@gmail.com>
2013-09-17 11:56:43 -0700

    Delete tvFeed.php
    moved to feed.php

--- tvFeed.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_URI'] == '/copperrun/tvFeed.php'){
-    die("Not allowed back here!");
-}
-//include 'functions.php';
-echo "<h1>HDTV Support brought to you by ....</h1>";
-//echo $programingCredit;
-?>
-This is a test of the refresh rate of the application
