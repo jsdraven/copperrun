47539b20bf8afd3c7a9d30e46b51e465fe1fdcac
Justin Scott <jjustin@gmail.com>
2013-09-19 18:24:34 -0700

    'stuff'

--- raceCat.php
+++ raceCat.php
@@ -2,15 +2,62 @@
 /*if ($_SERVER['REQUEST_URI'] == '/copperrun/raceCat.php'){
     die("Not allowed back here!");
 }*/
- $body .= "
-Race Catigory Setup and Edit
 
-<hr />
-<form action='index.php' method='POST'>
-Gender: <label>Male<input type='radio' name='gen' value='male' checked="" /></label> or Female<input type='radio' name='gen' value='female' /></label><br />
-<label>Min Age<input type='number' maxlength='2' name='minAge' /></label> --- <label>Max Age<input type='number' maxlength='3' name='maxAge'></label><br />
-<select name='raceType'>
-	<option>
+if (isset($list['gender']) && $list['gender'] == 'male') {
+	# code...
+	$genderSet = "<label>Male<input type='radio' name='gen' value='male' checked=\"\" /></label> or Female<input type='radio' name='gen' value='female' /></label>";
+}elseif (isset($list['gender']) && $list['gender'] == 'female') {
+	# code...
+	$genderSet = "<label>Male<input type='radio' name='gen' value='male' /></label> or Female<input type='radio' name='gen' value='female' checked=\"\" /></label>";
+}else{
+	$genderSet = "<label>Male<input type='radio' name='gen' value='male' /></label> or Female<input type='radio' name='gen' value='female' /></label>";
+}
+
+
+if (isset($list['type']) && $list['type'] == 'twoMile') {
+	# code...
+	$options = "	<option>
+		Choose one.
+	</option>
+	<option value='twoMile' selected=\"\">
+		Two Mile
+	</option>
+	<option value='halfMile'>
+		Half Mile
+	</option>
+	<option value='tenK'>
+		Ten K
+	</option>";
+}elseif (isset($list['type']) && $list['type'] == 'halfMile') {
+	# code...
+	$options = "	<option>
+		Choose one.
+	</option>
+	<option value='twoMile'>
+		Two Mile
+	</option>
+	<option value='halfMile' selected=\"\">
+		Half Mile
+	</option>
+	<option value='tenK'>
+		Ten K
+	</option>";
+}elseif (isset($list['type']) && $list['type'] == 'tenK') {
+	# code...
+	$options = "	<option>
+		Choose one.
+	</option>
+	<option value='twoMile'>
+		Two Mile
+	</option>
+	<option value='halfMile'>
+		Half Mile
+	</option>
+	<option value='tenK' selected=\"\">
+		Ten K
+	</option>";
+}else{
+	$options = "	<option>
 		Choose one.
 	</option>
 	<option value='twoMile'>
@@ -21,7 +68,20 @@ Gender: <label>Male<input type='radio' name='gen' value='male' checked="" /></la
 	</option>
 	<option value='tenK'>
 		Ten K
-	</option>
+	</option>";
+}
+
+
+ $body .= "
+
+Race Catigory Setup and Edit
+
+<hr />
+<form action='index.php' method='POST'>
+Gender: $genderSet<br />
+<label>Min Age<input type='number' maxlength='2' name='minAge' /></label> --- <label>Max Age<input type='number' maxlength='3' name='maxAge'></label><br />
+<select name='raceType'>
+$options
 </select>
 <input type='submit' name='setCat' value='Submit' />
 </form>
