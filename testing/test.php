84314718b29d5fbfdc335271cbe78ef0f99abb8c
unknown <ittemp@FD0LT20.FosterDairyFarms.com>
2014-01-09 16:39:02 -0800

    still working on runner search, and now working directly to submit or insert a php object. last left off trying to convert it into a sql friendly string.

--- testing/test.php
+++ testing/test.php
@@ -1,5 +1,71 @@
 <?php
-if ($_SERVER['REQUEST_URI'] == $_SERVER['PHP_SELF'] && $_SERVER['PHP_SELF'] != ){
-    die("Not allowed back here!");
+
+define('dbUser', 'root');
+define('dbPass', '');
+define('dbHost', 'localhost');
+define('dbname', 'mytesting');
+
+function DbConnection($query){
+
+    $DB_User = constant('dbUser');
+    $DB_Password = constant('dbPass');
+    $DB_Host = constant('dbHost');
+    $DB_Name = constant('dbname');
+    $copperrun = mysqli_connect($DB_Host, $DB_User, $DB_Password, $DB_Name);
+    $result = mysqli_query($copperrun, $query);
+    if(!$result){
+    	$end = "MySQL Failed to comply: ".mysqli_error($copperrun);
+    }else{
+    	$end = $result;
+    }
+    mysqli_close($copperrun);
+    return $end;
 }
-print_r($_SERVER);
+$sample = new stdClass();
+$sample->name = new stdClass();
+$Stuff = new stdClass();
+$Stuff->tenk = new stdClass();
+$Stuff->half = new stdClass();
+$Stuff->two = new stdClass();
+
+$Stuff->tenk->oPlace = 5;
+$Stuff->tenk->cPlace = 1;
+$Stuff->tenk->catID = 4;
+
+$Stuff->half->oPlace = 0;
+$Stuff->half->cPlace = 0;
+$Stuff->half->catID = 0;
+
+$Stuff->two->oPlace = 0;
+$Stuff->two->cPlace = 0;
+$Stuff->two->catID =0;
+
+$sample->name->first = 'justin';
+$sample->name->last = 'scott';
+$sample->age = 32;
+$sample->raceInfo = $Stuff;
+$blob = addslashes(serialize($Stuff));
+var_dump($blob);
+/*
+var_dump($sample);
+
+
+$blob = base64_encode($blob);
+$sql = "INSERT INTO test (name, object) VALUES('Justin', '$blob')";
+$insert = DbConnection($sql);
+var_dump($insert);
+
+
+*/
+
+
+$sql2 = "SELECT * FROM test WHERE name = 'Justin'";
+
+$result = DbConnection($sql2);
+$something = mysqli_fetch_object($result);
+var_dump($something);
+$returnedObject = stripslashes($something->object);
+var_dump($returnedObject);
+$returnedObject = unserialize($returnedObject);
+
+var_dump($returnedObject);
